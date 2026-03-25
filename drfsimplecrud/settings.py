e2530af42f4b6c511cbf2da01e3a42515bd96bf2
DanielBD117 <danielbustosduarte@gmail.com>
2026-03-25 18:03:13 -0500

    mensaje

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -28,16 +28,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = True
 DEBUG = 'RENDER' not in os.environ
 
-# Reemplaza las líneas 11 a 17 por esto:
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
-# Agrega siempre el dominio específico de tu app como respaldo
-ALLOWED_HOSTS.append('drfsimpletest-139y.onrender.com')
-ALLOWED_HOSTS.append('0.0.0.0') # Esto ayuda con el escaneo de puertos de Render
 
 # Application definition
 
