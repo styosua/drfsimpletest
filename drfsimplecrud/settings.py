5e4b70c9ba9cc1b9485b313145d7465bd5009ed3
DanielBD117 <danielbustosduarte@gmail.com>
2026-03-25 17:28:25 -0500

    mensaje

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
